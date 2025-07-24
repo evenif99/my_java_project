ff0b45a78cf97d921246c0a764a8029cc2898bb7
evenif99 <back86538774@gmail.com>
2025-07-24 16:52:24 +0900

    진짜 첫번째 연습

--- src/ch01_variable_operator/TernaryOperator.java
+++ src/ch01_variable_operator/TernaryOperator.java
@@ -19,5 +19,13 @@ public class TernaryOperator {
         String str = y%x == 0 ? "yes" : "no";
         System.out.println("약수 : " + str);
 
+        int su = 7 ;
+        // 숫자 7은(는) 홀수입니다.
+        String name = su%2 == 1 ? "홀수" : "짝수";
+        System.out.println("숫자 " + su + "는 " + name + " 입니다");
+        int score = 85 ; // 60점 이상이면 합격
+        String scr = score >= 60 ? "합격" : "불합격";
+        System.out.println(score + "점이므로 " + scr + " 입니다.");
+        // 85점이므로 합격입니다.
     }
 }
