4d9cc75ceb67b6314ddf15568561f7143f160525
evenif99 <back86538774@gmail.com>
2025-07-25 16:07:02 +0900

    진짜 첫번째 연습

--- src/ch02_control_statement/Logical01.java
+++ src/ch02_control_statement/Logical01.java
@@ -2,15 +2,17 @@ package ch02_control_statement;
 
 public class Logical01 {
     public static void main(String[] args) {
-        int season = 10;
-        if (season>=3 && season<=5) {
-            System.out.println("봄");
-        }else if (season>=6 && season <=8){
-            System.out.println("여름");
-        }else if (season>=9 && season<=11){
-            System.out.println("가을");
+        int month = 10;
+        String season ;
+        if (month >=3 && month <=5) {
+            season = "봄";
+        }else if (month >=6 && month <=8){
+            season = "여름";
+        }else if (month >=9 && month <=11){
+            season = "가을";
         } else{
-            System.out.println("겨울");
+            season = "겨울";
         }
+        System.out.println(month + "월은 " + season + "입니다.");
     }
 }
